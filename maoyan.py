600e2192856a9d8bb35cad0af5eb251fd4e6dc41
Your Name <email@example.com>
2018-06-12 10:34:05 +0800

    first commit

---
+++ maoyan.py
@@ -0,0 +1,41 @@
+import requests,re,json
+from requests.exceptions import RequestException
+#异常处理（错误返回None）
+def get_one_page(url, headers):
+    try:
+        response = requests.get(url,headers=headers)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+#获取第一页信息
+def parse_one_page(html):
+    pattern = re.compile(r'<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)</a>.*?name">(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>.*?integer">(.*?)</i>.*?fraction">(.*?)</i>', re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield{
+            'index' : item[0],
+            'image' : item[1],
+            'title' : item[2],
+            'actor' : item[3].strip()[3:],
+            'time' : item[4].strip()[5:],
+            'score' : item[5] + item[6]
+        }
+#保存到文件
+def save_to_file(content):
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content,ensure_ascii=False) + '\n')
+        f.close()
+
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'}
+    html = get_one_page(url,headers)
+    for item in parse_one_page(html):''
+        print(item)
+        save_to_file(item)
+
+if __name__ == '__main__':
+    for i in range(10):
+        main(i*10)
